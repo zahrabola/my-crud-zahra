6d827ee0bb465ab91e56f43e84c9b57f3e5589e1
zahrabola <95353397+zahrabola@users.noreply.github.com>
2024-07-11 13:41:36 +0100

    update

--- src/Users/UserActions.js
+++ src/Users/UserActions.js
@@ -3,7 +3,15 @@ import React from 'react';
 const UserActions = () => {
     return (
         <div>
-            UserActions
+            <button className='btn btn-success'>
+            <i className='pi pi-eye'></i>
+            </button>
+            <button className='btn btn-primary'>
+            <i className='pi pi-file-edit'></i>
+            </button>
+            <button className='btn btn-danger'>
+            <i className='pi pi-trash'></i>
+            </button>
         </div>
     );
 }
